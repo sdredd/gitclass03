efdd9e0e1ccadeee2fc4eab61025ca4ef2469527
sdredd <sdredd@gmail.com>
2022-05-31 18:54:43 -0500

    2nd commit

--- src/github/gitAndGithub.java
+++ src/github/gitAndGithub.java
@@ -3,7 +3,9 @@ package github;
 public class gitAndGithub {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("today we learn github");
+		System.out.println("today is a very good DAY");
 
 	}
 
